b9b474485fb6fab232ad89a596268cf28529bcaa
Erick <erickmartinlozano@outlook.com>
2022-01-26 17:51:45 -0600

    First commit

---
+++ src/Saludo/HolaMundo.java
@@ -0,0 +1,14 @@
+package Saludo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String saludo;
+		System.out.println("Hola Mundo");
+ 	System.out.println("Jayyyeaah");
+ 		
+	}
+
+}
